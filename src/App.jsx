e3995ffd8c49f58859e93e0556caebad3d98770d
Leslie Bagalho <leslie.bagalho@yahoo.com.br>
2025-08-08 21:26:31 -0300

    subindo projeto

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import './App.css'
+import CadastroGeral from './components/CadastroGeral'
+
+function App() {
+  return (
+    <>
+      <CadastroGeral />
+    </>
+  )
+}
+
+export default App
---
+++ src/components/CadastroGeral.jsx
@@ -0,0 +1,70 @@
+// src/components/CadastroGeral.jsx
+
+import React, { useState } from 'react';
+
+const CadastroGeral = () => {
+  const [nome, setNome] = useState('');
+  const [email, setEmail] = useState('');
+  const [senha, setSenha] = useState('');
+  const [mensagem, setMensagem] = useState('');
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    if (!nome || !email.includes('@') || senha.length < 6) {
+      setMensagem('Todos os campos devem estar preenchidos');
+      return;
+    }
+
+    setMensagem('Cadastro realizado com sucesso!');
+    setNome('');
+    setIdade('');
+    setEmail('');
+  };
+
+  return (
+    <div style={{ maxWidth: '400px', margin: '0 auto' }}>
+      <h2>Cadastro de Usuário</h2>
+      <form onSubmit={handleSubmit}>
+        <div>
+          <label>Nome:</label>
+          <input
+            type="text"
+            value={nome}
+            onChange={(e) => setNome(e.target.value)}
+            placeholder="Nome"
+            required
+          />
+        </div>
+
+        <div>
+          <label>E-mail:</label>
+          <input
+            type="email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+            placeholder="Email"
+            required
+          />
+        </div>
+
+        <div>
+          <label>Senha:</label>
+          <input
+            type="password"
+            value={senha}
+            onChange={(e) => setSenha(e.target.value)}
+            placeholder="Senha"
+            required
+          />
+        </div>
+
+        <button type="submit">Cadastrar</button>
+      </form>
+
+      {mensagem && <p>{mensagem}</p>}
+    </div>
+  );
+};
+
+export default CadastroGeral
